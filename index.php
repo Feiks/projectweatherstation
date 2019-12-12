47506f53d7410a20be4e828bcd12b321f09f777a
Bwise1 <oguntoyebenjamin2@gmail.com>
2019-12-12 12:17:46 +0200

    update

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:appt.database.windows.net,1433; Database = ApptSystem", "eughami", "{Farah123}");
+    $conn = new PDO("sqlsrv:server = tcp:digitalstation.database.windows.net,1433; Database = digitalstation", "feliks", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "eughami", "pwd" => "Farah123", "Database" => "ApptSystem", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:appt.database.windows.net,1433";
+$connectionInfo = array("UID" => "feliks", "pwd" => "{your_password_here}", "Database" => "digitalstation", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:digitalstation.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
